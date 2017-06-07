3eef65d374d44020df379e959ae7d336beb7b339
Shscherbina Elena <olena.scherbina@gmai.com>
2017-06-07 22:32:42 +0300

    added new method for pause

--- js/script1.js
+++ js/script1.js
@@ -1,55 +1,65 @@
 (function (){
 	'use strict';
 
-var timer = function() {
+var TIMER = function() {
 	this.currentTime = 0;
 	this.startTime = 0;
 	this.deltaTime = 0;
 	this.isActive = false;
 	this.interval = 0;
+	this.value = 0;
+	this.startBtn;
+	this.stopBtn;
 	this.elements = {};
 
 	this.createLayout = function() {
 			var container = createNode('div', null, null, null);
 			container.style.border = '1px solid #ccc';
-			var startBtn = createNode('button', null, null, 'START'),
-		  	  stopBtn = createNode('button', null, null, 'STOP');
+			this.startBtn = createNode('button', null, null, 'START'),
+		  this.stopBtn = createNode('button', null, null, 'STOP');
 		 	this.elements.timeField = createNode('p', null, null, '0');
 			this.elements.timeField.style.fontSize = '3rem';
-		 	startBtn.addEventListener('click', function(){
+		 	this.startBtn.addEventListener('click', function(){
 				this.startTimer();
 			}.bind(this));
-			stopBtn.addEventListener('click', function(){
+			this.stopBtn.addEventListener('click', function(){
 				this.stopTimer();
 			}.bind(this));
-			container.append(startBtn, stopBtn, this.elements.timeField);
+			container.append(this.startBtn, this.stopBtn, this.elements.timeField);
 			console.log(container);
 			return container;
 		};
 
 		this.startTimer = function() {
 			if(!this.isActive){
-				 this.createLayout.startBtn = createNode('button', null, null, 'PAUSE');
+				var self = this;
+console.log(self.startBtn);
+				 this.startBtn.innerHTML = 'PAUSE';
 				 this.setTime();
-				 console.log(this.startTime);
 				 this.runTime();
 				 this.isActive = true;
 			 } else {
+				 //this.stop();
 				 this.isActive = false;
-				 this.createLayout.startBtn = createNode('button', null, null, 'CONTINUE');
+				 //this.currentTime += this.deltaTime;
+				 this.startBtn.innerHTML = 'CONTINUE';
 				 clearInterval (this.interval);
 		 }
 		};
 
 		this.stopTimer = function () {
 			clearInterval (this.interval);
-			this.createLayout.startBtn.innerHTML = 'Start';
+			this.startBtn.innerHTML = 'START';
+			//this.value += this.deltaTime;
 			this.isActive = false;
 	};
 
 		this.runTime = function () {
-			this.interval = setInterval (function(){
-				this.updateTime();
+			var self = this;
+			console.log('self', self);
+			self.currentTime += self.deltaTime;
+			self.interval = setInterval (function(){
+				self.updateTime();
     	}, 1);
 		};
 
@@ -62,12 +72,14 @@ var timer = function() {
 				+ this.deltaTime.getMinutes() + ' : '
 				+ this.deltaTime.getSeconds() + ' : '
 				+ this.deltaTime.getMilliseconds();
+			console.log('hours', this.deltaTime.getHours())
 		};
 
 	this.updateTime = function() {
 			this.currentTime = Date.now();
+			console.log ('currentTime', this.currentTime);
+			console.log('startTime', this.startTime);
 			this.deltaTime = new Date(this.currentTime - this.startTime);
-			console.log(this.deltaTime);
 			this.updateHTML();
 		};
 
@@ -78,7 +90,7 @@ var timer = function() {
 
 
 };
-		var instance1 = new timer();
+		var instance1 = new TIMER();
 		console.log(instance1);
 		instance1.init();
 })();
